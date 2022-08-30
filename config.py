b15f7051932772d5600953809bca29ef41a6b412
DewKaRoh <78058999+DewKaRoh@users.noreply.github.com>
2022-08-30 14:19:17 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cayyrjnl7stv.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "dewkaroh-employee"
+customregion = "us-east-1"
