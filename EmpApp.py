7007a2427b2423ccc962ba17f7dfac4327309165
DewKaRoh <78058999+DewKaRoh@users.noreply.github.com>
2022-09-28 05:52:52 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -23,15 +23,31 @@ table = 'employee'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('AddEmp.html')
+    return render_template('HRMain.html')
 
 
 @app.route("/about", methods=['POST'])
 def about():
     return render_template('www.intellipaat.com')
 
+@app.route("/registerEmp", methods=['GET', 'POST'])
+def registerEmp():
+    return render_template('RegisterEmp.html')
 
-@app.route("/addemp", methods=['POST'])
+@app.route("/searchEmp", methods=['GET', 'POST'])
+def searchEmp():
+    return render_template('SearchEmp.html')
+
+@app.route("/backMain", methods=['GET', 'POST'])
+def backMain():
+    return render_template('HRMain.html')
+
+@app.route("/search", methods=['GET', 'POST'])
+def search():
+    return render_template('DisplayEmp.html')
+
+
+@app.route("/addemp", methods=['POST', 'GET'])
 def AddEmp():
     emp_id = request.form['emp_id']
     first_name = request.form['first_name']
@@ -55,30 +71,14 @@ def AddEmp():
         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
         s3 = boto3.resource('s3')
 
-        try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
-
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location
-
-            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                emp_image_file_name_in_s3)
-
-        except Exception as e:
-            return str(e)
-
     finally:
         cursor.close()
 
     print("all modification done...")
     return render_template('AddEmpOutput.html', name=emp_name)
+
+
+ 
 
 
 if __name__ == '__main__':
